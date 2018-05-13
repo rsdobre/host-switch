b049a17e3f2eab262c562dad8059b6d1dfe9f3e5
Unknown <rares.dobre@gmail.com>
2018-05-13 10:07:24 +0300

    Added basic functionality

---
+++ off.cmd
@@ -0,0 +1,4 @@
+@echo off
+echo "Welcome, I'll turn off your hosts file now!"
+pause
+rename C:\Windows\System32\drivers\etc\hosts nothosts
---
+++ on.cmd
@@ -0,0 +1,4 @@
+@echo off
+echo "Welcome, I'll turn on your hosts file now!"
+pause
+rename C:\Windows\System32\drivers\etc\nothosts hosts
